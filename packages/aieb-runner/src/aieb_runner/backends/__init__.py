42df4b225522cfdda37e7282ad96968d07c440c9
Haisam Abbas <HaisamAbbas@outlook.com>
2026-09-22 01:07:16 +0500

    Export Harbor result normalization boundary

--- packages/aieb-runner/src/aieb_runner/backends/__init__.py
+++ packages/aieb-runner/src/aieb_runner/backends/__init__.py
@@ -1,4 +1,13 @@
 """Backend-neutral execution contracts."""
 from .orchestration import BackendExecutionError, BackendRunResult, run_bounded
+from .normalization import HarborResultError, NormalizedHarborResult, execute_and_normalize, normalize_harbor_result
 
-__all__ = ["BackendExecutionError", "BackendRunResult", "run_bounded"]
+__all__ = [
+    "BackendExecutionError",
+    "BackendRunResult",
+    "HarborResultError",
+    "NormalizedHarborResult",
+    "execute_and_normalize",
+    "normalize_harbor_result",
+    "run_bounded",
+]
